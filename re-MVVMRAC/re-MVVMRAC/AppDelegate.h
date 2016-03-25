c0fd61fa9808c05240c8169040d30cec2958592a
jiangpc <pengcheng.jiang@pilot-lab.com.cn>
2016-03-25 16:12:23 +0800

    re-MVVMRAC

---
+++ re-MVVMRAC/re-MVVMRAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  re-MVVMRAC
+//
+//  Created by PilotLabHome on 16/3/25.
+//  Copyright © 2016年 PilotLabHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
